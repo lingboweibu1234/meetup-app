64a920073a32797d6738bada30d32bb9a381cce9
lingboweibu1234 <qawzse656746@gmail.com>
2017-07-28 10:05:05 +0800

    add model controller

---
+++ db/migrate/20170728014717_create_meetups.rb
@@ -0,0 +1,9 @@
+class CreateMeetups < ActiveRecord::Migration[5.0]
+  def change
+    create_table :meetups do |t|
+      t.string   "title"
+      t.string   "description"
+      t.timestamps
+    end
+  end
+end
